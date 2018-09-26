50a95865cb36859ae209f1cf08b0960008d1dee4
parkito <artyom-karnov@yandex.ru>
2018-09-27 01:44:33 +0300

    Change project structure

--- client/src/main/java/com/roommate/basecontrol/client/api/GroupClientService.java
+++
@@ -1,9 +0,0 @@
-package com.roommate.basecontrol.client.api;
-
-/**
- * @author Artem Karnov @date 04.04.17.
- *         artem.karnov@t-systems.com
- */
-
-public interface GroupClientService {
-}
--- controller/src/main/java/com/roommate/basecontrol/controllers/rest/GroupController.java
+++
@@ -1,9 +0,0 @@
-package com.roommate.basecontrol.controllers.rest;
-
-/**
- * @author Artem Karnov @date 07.04.17.
- *         artem.karnov@t-systems.com
- */
-
-public class GroupController {
-}
